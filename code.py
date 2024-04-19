e5e02f47afd5e8a7a8d02b6f4ddd845962971804
jing.....菁 <77629833+jingblog@users.noreply.github.com>
2024-04-19 20:56:43 +0800

    Update code.py

--- code.py
+++ code.py
@@ -10,32 +10,31 @@ headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55"
 }
  
-# 1. Website Parsing
-# Using a web scraper to fetch all COVID-19 infection data for the United Kingdom.
+# 网站解析
 name = "us"
-url = 'https://www.worldometers.info/coronavirus/' + name + "/"
+url = 'https://www.worldometers.info/coronavirus/'
 res = requests.get(url=url, headers=headers).content.decode()
 html = etree.HTML(res)
 
-# Representing the total number of infections in the United States, daily new confirmed cases, current confirmed cases, total deaths, and daily new deaths.
+# 分别表示美国总感染人数，单日新增确诊人数，现有确诊人数，总死亡人数，单日新增死亡人数
 fname=["Total Cases of " + name,"Daily New Cases of " + name,"Active Cases of " + name,"Total Deaths of " + name,"Daily Deaths of " + name]
  
 
-# open file
+# 打开文件
 tname = "true data.csv"
 if os.path.exists(tname):
     os.remove(tname)
 f=open(tname, "a", newline="")
 writer = csv.writer(f)
 
-# 2. try to Send Request
+# 尝试发送请求
 try:
     data = html.xpath(f'/html/body/div/div[2]/div[1]/div[1]/div/script/text()')[0]
 except:
-    print("Data retrieval failed—XPath error",name)
+    print("数据获取失败----xpath错误",name)
     sys.exit()
 
-# Get Date
+# 获取日期
 compiles_date = re.compile("categories: \[(.*?)\]")
 data1=compiles_date.findall(data)[0]
 lex = shlex.shlex(data1)
@@ -44,48 +43,47 @@ lex.quotes='"'
 lex.whitespace_split = True
 itemlist=list(lex)
 
-# Remove the " symbol
+# 把 " 去掉
 for i in list(range(0, len(itemlist))):
     itemlist[i]=itemlist[i].strip('"')
 date=itemlist
 date.insert(0,"")
-# Writing Date
+# 写入日期
 try:
-    #print(date)
     writer.writerow(date)
 except:
-    print("Writing Date Failed",name)
+    print("写入日期失败",name)
     sys.exit()
 
-# 3. Iterating through each set of data
+# 循环遍历每一组数据
 for choice in range(0,5,1):
-    # Attempting to Send Request
+    # 尝试发送请求
     try:
         data = html.xpath(f'/html/body/div/div[2]/div[1]/div[{choice+1}]/div/script/text()')[0]
     except:
-        print("Data retrieval failed—XPath error",fname[choice])
+        print("数据获取失败----xpath错误",fname[choice])
         sys.exit()
 
-    # Regex Matching
+    # 正则匹配
     compiles_count = re.compile("data: \[(.*?)\]")
 
-    # Attempting to Send Request
+    # 尝试发送请求
     try:
         counts = compiles_count.findall(data)[0]
     except:
-        print("Data retrieval failed—Data regex matching error",fname[choice])
+        print("数据获取失败----data正则匹配错误",fname[choice])
         sys.exit()
 
-    # Splitting into Lists
+    # 分割成列表
     counts_list=counts.split(",")
     counts_list.insert(0,fname[choice])
-    # Writing Infected Cases
+    # 写入感染人数
     try:
         writer.writerow(counts_list)
     except:
-        print("Writing Case Numbers Failed",fname[choice])
+        print("写入人数失败",fname[choice])
         sys.exit()
 
-    print("Data Retrieval Successful", fname[choice])
-# Close File
+    print("数据获取成功", fname[choice])
+# 关闭文件
 f.close()
